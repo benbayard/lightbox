26e4b8b98d447c050e40699b44687bb0b86c9726
Ben Bayard <bjbayard@gmail.com>
2017-03-14 22:53:43 -0700

    Ensure size of images is at least usable.

--- src/js/config/google.js
+++ src/js/config/google.js
@@ -1,8 +1,8 @@
 export const URI = (
-  query = "Puppies",
+  query = "1920x1080 dog",
   key = "AIzaSyBQk73LkIJBLB_O25Ro6q795ks8DWYQAOw",
   context = "001532126640155556502:f9o-ifgnlek"
-) => `https://www.googleapis.com/customsearch/v1?q=${query}&key=${key}&cx=${context}&searchType=image&imgSize=xxlarge`;
+) => `https://www.googleapis.com/customsearch/v1?q=${query}&key=${key}&cx=${context}&searchType=image`;
 
 export const cookDataFormat = ({items}) =>
   items.map(({title, link}) => ({
